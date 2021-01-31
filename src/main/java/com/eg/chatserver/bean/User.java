67edb8bc78318e3baf81f452fb947e321b6782fc
makewheels <makewheels>
2021-01-31 23:31:34 +0800

    最简化xml

---
+++ src/main/java/com/eg/chatserver/bean/User.java
@@ -0,0 +1,105 @@
+package com.eg.chatserver.bean;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String userId;
+
+    private String loginName;
+
+    private String password;
+
+    private String loginToken;
+
+    private String jpushRegistrationId;
+
+    private String headImageUrl;
+
+    private String nickname;
+
+    private String phone;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName == null ? null : loginName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getLoginToken() {
+        return loginToken;
+    }
+
+    public void setLoginToken(String loginToken) {
+        this.loginToken = loginToken == null ? null : loginToken.trim();
+    }
+
+    public String getJpushRegistrationId() {
+        return jpushRegistrationId;
+    }
+
+    public void setJpushRegistrationId(String jpushRegistrationId) {
+        this.jpushRegistrationId = jpushRegistrationId == null ? null : jpushRegistrationId.trim();
+    }
+
+    public String getHeadImageUrl() {
+        return headImageUrl;
+    }
+
+    public void setHeadImageUrl(String headImageUrl) {
+        this.headImageUrl = headImageUrl == null ? null : headImageUrl.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
